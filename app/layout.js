3dae4e331d0efe02ec1aa45a2b4a8a662c88657f
Firzus <lprieu@gaming.tech>
2024-02-10 17:57:46 +0100

    Add SpeedInsights component to RootLayout

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,5 @@
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import { Inter } from "next/font/google";
 import "@/styles/globals.css";
 
@@ -14,6 +15,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={inter.className}>{children}</body>
       <Analytics />
+      <SpeedInsights />
     </html>
   );
 }
